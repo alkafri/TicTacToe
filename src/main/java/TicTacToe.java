103b8516a095932a434c521a27b6deda9c432510
Sigge <saorust@hotmail.com>
2022-12-01 04:08:25 +0100

    First commit

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class TicTacToe {
+package PACKAGE_NAME;
+
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.scene.input.MouseButton;
+
+public class TicTacToe {
+
+    private boolean turnX = true;
+
+    private boolean playable = true;
+
+
+
+    private class Tile extends StackPane { // Guess stackpane?
+        private Text text = new Text();
+
+        public Tile() {
+            setOnMouseClicked(event -> {
+                if (!playable)
+                    return;
+
+                if (event.getButton() == MouseButton.PRIMARY) {
+                    if (turnX) {
+                        drawX();
+                    } else {
+                        drawO();
+                    }
+
+                    // Some form of check to se if it was a winner move?
+                }
+            });
+        }
+        private void drawX() {
+            text.setText("X");
+        }
+
+        private void drawO() {
+            text.setText("O");
+        }
+
+    }
+
+
 }
